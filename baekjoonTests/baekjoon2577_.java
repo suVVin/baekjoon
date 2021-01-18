096835db64a17f13afe2150b87ae47ad4f07cf71
suVVin <suvvin97@gmail.com>
2021-01-18 23:03:01 +0900

    어렵다 악!

--- baekjoonTests/baekjoon2577_.java
+++ baekjoonTests/baekjoon2577_.java
@@ -13,14 +13,23 @@ public class baekjoon2577_ {
 		int B = Integer.parseInt(br.readLine());
 		int C = Integer.parseInt(br.readLine());
 		int D = A*B*C;
-		int Dlength = (int)(Math.log10(D)+1); 
-		//(int)(Math.log10(D)+1)은 숫자의 자리수 구하는 방법
-				
-		for(int i=0; i<Dlength; ++i) {
-			
-				
-			}
-		}
 		
+		int [] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+		
+		for(int i=0; i<array.length; ++i) {
+			int count=0; //일치하는 숫자를 세어 저장할 변수
+			int temp = D; //입력 숫자를 복사
+			
+			while(temp !=0) {
+				if (temp%10==array[i]) { //끝자리 수부터 비교
+					count++;
+				}
+				temp /=10;
+			}
+			bw.write(count);
+		}
+		br.close();
+		bw.flush();
+		bw.close();
 	}
 }
