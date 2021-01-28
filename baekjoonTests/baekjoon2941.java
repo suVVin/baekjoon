be4facdbabbc4ffe98c0d7c0e371640cc9b1e272
suVVin <suvvin97@gmail.com>
2021-01-28 21:00:15 +0900

    21/01/28

---
+++ baekjoonTests/baekjoon2941.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class baekjoon2941 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String S = sc.next().toLowerCase();
+		int cnt = 0;
+		sc.close();
+
+		if (S.charAt(i) == 'č' || S.charAt(i) == 'ć') {
+			'č'='c\='; 'ć'='c-';
+		for (int i = 0; i < S.length(); i++) {
+			
+			}
+		}
+
+	}
+}
