6bbd9ad0f96da5fd9a57cf4461af866b71dd73f4
suVVin <suvvin97@gmail.com>
2021-01-29 20:38:33 +0900

    21/01/29 하기가 싫어요 ㅠ ㅠ

--- baekjoonTests/baekjoon2941.java
+++ baekjoonTests/baekjoon2941.java
@@ -8,12 +8,27 @@ public class baekjoon2941 {
 		int cnt = 0;
 		sc.close();
 
-		if (S.charAt(i) == 'č' || S.charAt(i) == 'ć') {
-			'č'='c\='; 'ć'='c-';
-		for (int i = 0; i < S.length(); i++) {
-			
-			}
-		}
+		if (S.charAt(i) == 'č' || S.charAt(i) == 'ć') 
+			'č'='c'; 'ć'='c';
+			if (S.charAt(i) == 'dž')
+				char dž = 'd';
+			if (S.charAt(i) =='đ')
+				'đ'= 'd';
+			if (S.charAt(i) == 'š')
+				'š' = 's';
+			if (S.charAt(i) =='ž')
+				'ž' = 'z';
+			if (S.charAt(i) =='lj')
+				'lj' = 'l'
+			if (S.charAt(i) =='nj')
+				'nj'= 'n'
+			for (int i = 0; i < S.length(); i++) {
+				for(char j = a; j<=z; j++) {
+					if (S.charAt(i) = j)
+						cnt ++;
+				}
 
+			}
+			System.out.println(cnt);
 	}
 }
